112dde63b77c81198f8fce30c21a21b3e1427606
刘阳 <numbksco@gmail.com>
2021-11-23 22:52:29 +0800

    refactor: if statement

--- cc/codegen.go
+++ cc/codegen.go
@@ -13,7 +13,7 @@ const StackSize = 65536
 type Codegen struct {
 	objects    []*Object
 	depth      int
-	labelCount int
+	blockCount int
 	writer     io.Writer
 }
 
@@ -77,14 +77,24 @@ func (c *Codegen) GenCode() {
 			c.Printf(" (param $%s %s)", param.Name, param.Type.WasmType())
 		}
 		c.Printf(" (result i32)\n")
+		c.Printf("    (local $result i32)\n")
 
 		// Prologue
 		c.Printf("    global.get $sp\n")
 		c.Printf("    i32.const %d\n", o.Function.StackSize)
 		c.Printf("    i32.sub\n")
 		c.Printf("    global.set $sp\n")
+		c.Printf("    block $ENTRY\n")
 
 		c.GenStmt(o.Function.Body)
+
+		// // Epilogue
+		c.Printf("    end\n")
+		c.Printf("    global.get $sp\n")
+		c.Printf("    i32.const %d\n", o.Function.StackSize)
+		c.Printf("    i32.add\n")
+		c.Printf("    global.set $sp\n")
+		c.Printf("    local.get $result\n")
 		c.Printf("    return\n")
 		c.Printf("  )\n")
 	}
@@ -92,6 +102,23 @@ func (c *Codegen) GenCode() {
 
 func (c *Codegen) GenStmt(node *Node) {
 	switch node.Kind {
+	case NKIf:
+		blockFalseName := c.NextBlockName()
+		blockTrueName := c.NextBlockName()
+		c.Printf("    block %s\n", blockFalseName)
+		c.Printf("    block %s\n", blockTrueName)
+		c.GenExpr(node.IfClause.Cond)
+		c.Printf("    i32.eqz\n")
+		c.Printf("    br_if %s\n", blockTrueName)
+		c.GenStmt(node.IfClause.Then)
+		c.Printf("    br %s\n", blockFalseName)
+		c.Printf("    end\n")
+		if node.IfClause.Else != nil {
+			c.GenStmt(node.IfClause.Else)
+		}
+		c.Printf("    br %s\n", blockFalseName)
+		c.Printf("    end\n")
+		return
 	case NKBlock, NKStmtsExpr:
 		for _, n := range node.Block.Stmts {
 			c.GenStmt(n)
@@ -99,7 +126,9 @@ func (c *Codegen) GenStmt(node *Node) {
 		return
 	case NKReturn:
 		c.GenExpr(node.Unary.Expr)
-		c.Printf("    return\n")
+
+		c.Printf("    local.set $result\n")
+		c.Printf("    br $ENTRY\n")
 		return
 	case NKExprStmt:
 		c.GenExpr(node.Unary.Expr)
@@ -201,4 +230,10 @@ func (c *Codegen) GenAddr(node *Node) {
 	}
 
 	panic(errors.New("not a lvalue"))
+}
+
+func (c *Codegen) NextBlockName() string {
+	result := fmt.Sprintf("$B%d", c.blockCount)
+	c.blockCount++
+	return result
 }
--- tests/control_test.go
+++ tests/control_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestControl(t *testing.T) {
 	a := Assert{t: t}
-	a.Eval(3, "int main() { if (0) return 2; return 3; }")
-	a.Eval(3, "int main() { if (1-1) return 2; return 3; }")
-	a.Eval(2, "int main() { if (1) return 2; return 3; }")
-	a.Eval(2, "int main() { if (2-1) return 2; return 3; }")
+	a.Eval(int32(3), "int main() { if (0) return 2; return 3; }")
+	a.Eval(int32(3), "int main() { if (1-1) return 2; else return 3; }")
+	a.Eval(int32(2), "int main() { if (1) return 2; else return 3; }")
+	a.Eval(int32(2), "int x; int main() { if (2-1) x = 2; else x = 3; return x; }")
 
 	a.Eval(55, "int main() { int i=0; int j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }")
 	a.Eval(3, "int main() { for (;;) return 3; return 5; }")
